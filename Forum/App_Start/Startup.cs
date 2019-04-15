ac9c66f98e8464c7a52384667f6f0138ad4c9e3d
Anton <anton.komar2979@gmail.com>
2019-04-15 23:52:54 +0300

    Added "Startup" class with Owin and updated frameworks.

---
+++ Forum/App_Start/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Forum.Infrastructure.Business.Interfaces;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forum.App_Start
+{
+    public class Startup
+    {
+        IUserService userService;
+        public void Configuration(IAppBuilder app)
+        {
+            //Error 
+            //app.CreatePerOwinContext<IUserService>(userService); 
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Login"),
+            });
+        }
+    }
+}
